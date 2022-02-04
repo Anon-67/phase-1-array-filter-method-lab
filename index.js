334c742369f1380341be98bc7144b21a96118bdf
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-02-04 08:59:07 -0600

    commit tree f386b932c693c74ea910e4867a6d1a825fa068c6

--- index.js
+++ index.js
@@ -1,1 +1,94 @@
-// Code your solution here
+
+
+
+const findMatching = (arr, str) => arr.filter(function(ele) {
+        return ele.toLowerCase() === str.toLowerCase();
+    })
+
+function findMatching2(arr, str) {
+    return arr.filter(function (ele){
+        return ele.toLowerCase() === str.toLowerCase();
+    });
+}
+
+
+const fuzzyMatch = (arr, str) => arr.filter(function(ele) {
+    return ele.charAt(0) === str.charAt(0);
+});
+
+
+const matchName = (arr, str) => arr.filter(function(ele) {
+    return ele.name == str;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const drivers = [
+//     {
+//       name: 'Bobby',
+//       hometown: 'Pittsburgh' },
+//     {
+//       name: 'Sammy',
+//       hometown: 'New York' } ,
+//     {
+//       name: 'Sally',
+//       hometown: 'Cleveland' },
+//     {
+//       name: 'Annette',
+//       hometown: 'Los Angeles' },
+//     {
+//       name: 'Bobby',
+//       hometown: 'Tampa Bay' }
+//   ]
+
+
+
+
+
+
+// function findMatching(arr, str) {
+//     const newArr = arr.filter(function (ele) {
+//         return ele.name == str;
+//          });
+//     let secArr = [];
+//     newArr.forEach(function (ele) {
+//         secArr.push(ele.name)
+//     });
+//     console.log(secArr);
+//     return secArr;
+// };
+
+
+
+
+// // let thirdArr = [];
+
+// // secArr.forEach(function (ele) {
+// //     thirdArr.push(ele.name);
+// // });
+ 
+// findMatching(drivers, "Bobby");
+
+// // console.log(secArr);
+
+
+
+// // let fuckNose = [{ name: 'Bobby', hometown: 'Pittsburgh' }, { name: 'Bobby', hometown: 'Tampa Bay' }];
+
+// // console.log(fuckNose[0].name);
